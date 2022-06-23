dc1ef4f0cf90adfae81c4801e38b739434e82c83
Anders Logg <logg@chalmers.se>
2022-06-23 10:30:38 +0200

    Add protobuf compiler

--- docker/install_scripts/install_dependencies.sh
+++ docker/install_scripts/install_dependencies.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 apt-get update && apt-get install -y \
     build-essential \
     cmake\
     nano \
-    net-tools 
+    net-tools \
+    protobuf-compiler
 
 ./install_python.sh
 ./install_py_libs.sh
