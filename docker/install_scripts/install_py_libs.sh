6b74a7168567788cd327f118f1619efc9838e567
Anders Logg <logg@chalmers.se>
2022-06-23 10:41:18 +0200

    Use protobuf 3.20 due to following error message with latest package:
      File "/usr/local/lib/python3.8/dist-packages/google/protobuf/descriptor.py", line 560, in __new__
        _message.Message._CheckCalledFromGeneratedFile()
    TypeError: Descriptors cannot not be created directly.
    If this call came from a _pb2.py file, your generated code is out of date and must be regenerated with protoc >= 3.19.0.
    If you cannot immediately regenerate your protos, some other possible workarounds are:
     1. Downgrade the protobuf package to 3.20.x or lower.
     2. Set PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python (but this will use pure-Python parsing and will be much slower).

--- docker/install_scripts/install_py_libs.sh
+++ docker/install_scripts/install_py_libs.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python3 -m pip install redis pika numpy protobuf fastapi uvicorn[standard]
+
+python3 -m pip install \
+	redis \
+	pika \
+	numpy \
+	fastapi \
+	uvicorn[standard] \
+	protobuf==3.20.*
