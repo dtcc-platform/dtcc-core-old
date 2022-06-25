59c6d1ee31da9f2258b7f582f71030dbdd820c22
Dag Wästberg <dwastberg@gmail.com>
2022-06-25 12:44:48 +0200

    rename

--- unittests/test.py
+++ unittests/test.py
@@ -4,7 +4,7 @@ import sys
 from pathlib import Path
 
 sys.path.append( str((Path(__file__).parent / "../src").resolve() ))
-from dtcc import buildings
+from dtcc import cityModel
 
 class TestBuildings(unittest.TestCase):
 
@@ -12,9 +12,9 @@ class TestBuildings(unittest.TestCase):
     def setUpClass(cls):
         cls.building_shp_file = str((Path(__file__).parent / "data" / "MinimalCase" / "propertyMap.shp").resolve())
 
-    def test_load_buildings(self):
-        cityModel = buildings.LoadBuildings(self.building_shp_file,'uuid')
-        self.assertEqual(len(cityModel.buildings),5)
+    def test_load_shp_buildings(self):
+        cm = cityModel.LoadBuildings(self.building_shp_file,'uuid')
+        self.assertEqual(len(cm.buildings),5)
 
 if __name__ == '__main__':
     unittest.main()
