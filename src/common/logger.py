ead92fce4c7739fe1a9938d44b35d6ee255de08a
sidkas <sidkas@protonmail.ch>
2022-06-30 11:31:17 +0200

    copy file redis and logger

---
+++ src/common/logger.py
@@ -0,0 +1,62 @@
+import os, sys, pathlib, logging, logging.config, time, datetime, socket
+from logging.handlers import RotatingFileHandler
+
+
+project_folder_path = str(pathlib.Path(__file__).resolve().parents[2])
+sys.path.append(project_folder_path)
+
+
+log_format = "%(name)s - %(asctime)-15s %(levelname)-5s [%(filename)s:%(lineno)d] %(message)s"
+log_level = "DEBUG"
+
+logger_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": log_format
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": log_level,
+            "formatter": "simple",
+            "stream": "ext://sys.stdout"
+        }
+    },
+
+    "root": {
+        "level": log_level,
+        "handlers": ["console"]
+    }
+
+}
+
+def init_file_handler(logger,log_file_path,formatter):
+    try:
+        file_handler = RotatingFileHandler(
+            log_file_path,
+            maxBytes=2e+7,
+            backupCount=20
+        )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    except BaseException as e:
+        print(f"error - logger - {datetime.datetime.now().isoformat()} cannot add file handler: {str(e)}")
+    return logger
+
+
+def getLogger(logger_name, log_file_name='common.log'):
+    logger = logging.getLogger(logger_name)
+    formatter = logging.Formatter(log_format)
+
+    logging.config.dictConfig(logger_config)
+
+    log_file_path = os.path.join(project_folder_path,'logs',log_file_name)
+    os.makedirs(log_file_path,exist_ok=True)
+
+    # Rotating File handler
+    logger = init_file_handler(logger,log_file_path,formatter)
+
+    return logger
--- src/file_sharing.py
+++ src/file_sharing.py
@@ -1,4 +1,4 @@
-import os, pathlib, sys, argparse, threading, time
+import os, pathlib, sys, argparse, threading, time, shutil
 import subprocess
 from multiprocessing.pool import ThreadPool
 from dotenv import load_dotenv
@@ -8,21 +8,28 @@ from dotenv import load_dotenv
 project_dir = str(pathlib.Path(__file__).resolve().parents[1])
 sys.path.append(project_dir)
 
+from src.common.logger import getLogger
+
+logger = getLogger(__file__)
+
 load_dotenv(os.path.join(project_dir, 'docker','.env'))
 
 docker_working_dir = os.getenv('WORKING_DIR')
 shared_data_dir = os.getenv('SHARED_DATA_DIR')
 
-from src.redis_service import RedisPubSub
+from src.common.redis_service import RedisPubSub
 
 def load_sample_file(path:str):
     if os.path.exists(path):
         with open(path, mode='r') as f:
             lines = f.readlines()
-            print(lines)
+            logger.info(lines)
         
     else:
-        print(f"path doesn't exist {path}")
+        logger.error(f"path doesn't exist {path}")
+
+def run_binary(parameters_path:str):
+    pass
 
 def create_sample_file():
     file_path = os.path.join(shared_data_dir,'sample.txt')
@@ -46,11 +53,22 @@ def run_and_notify(host, port,channel,callback,callback_args=[]):
 
     published = rps.publish(channel=channel,message=str(file_path))
     if published:
-        print("published")
+        logger.info("published")
 
 
 
 
+parameters_file_path = os.path.join(project_dir, "unittests/data/MinimalCase/Parameters.json")
+destination_folder = os.path.join(shared_data_dir,'vasnas')
+
+def copy_and_notify(args):
+    rps = RedisPubSub(host=args.host,port=args.port)
+    src_file_path = args.src
+    dst_file_path = os.path.join(args.dst, os.path.split(src_file_path)[1])
+    shutil.copy(src_file_path, dst_file_path)
+    published = rps.publish(channel=args.channel, message=dst_file_path)
+    if published:
+        logger.info("published")
 
 if __name__=='__main__':
 
@@ -72,6 +90,14 @@ if __name__=='__main__':
     pub.add_argument("--host", "-H", type=str,default='redis', help="hostname")
     pub.add_argument("--port", "-P", type=int,default=6379, help="port")
     pub.add_argument("--channel", "-C", type=str,default='dtcc-core', help="redis channel")
+
+
+    cp = subparser.add_parser('copy')
+    cp.add_argument("--host", "-H", type=str,default='redis', help="hostname")
+    cp.add_argument("--port", "-P", type=int,default=6379, help="port")
+    cp.add_argument("--channel", "-C", type=str,default='dtcc-core', help="redis channel")
+    cp.add_argument("--src", type=str,default=parameters_file_path, help="source file path")
+    cp.add_argument("--dst", type=str,default=destination_folder, help="destination folder path")
     
     args = parser.parse_args()
 
@@ -82,7 +108,10 @@ if __name__=='__main__':
     if args.command == 'run':
         run_and_notify(host=args.host,port=args.port,channel=args.channel,callback=create_sample_file)
 
-    
+    if args.command == 'copy':
+        copy_and_notify(args)
+        
+
 
 
 
