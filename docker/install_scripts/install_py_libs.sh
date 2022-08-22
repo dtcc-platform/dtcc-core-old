21bf86574ab19a77123e8ca745eabb6bed687bcd
Dag Wästberg <dwastberg@gmail.com>
2022-08-22 16:58:35 +0200

    install shapely

--- docker/install_scripts/install_py_libs.sh
+++ docker/install_scripts/install_py_libs.sh
@@ -9,6 +9,7 @@ python3 -m pip install \
 	laspy[lazrs] \
 	protobuf==3.20.* \
 	h5py \
+	shapely \ 
 	pybind11
 
 # These libraries require all of gdal-dev to be apt installed if you want to install them via pip
