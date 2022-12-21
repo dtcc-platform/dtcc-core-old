6f7090ff2b19dc05f17b7bc70b7c7f430096daea
sidkas <sidkas@protonmail.ch>
2022-12-21 11:34:00 +0100

    scripts for dtcc modules

---
+++ scripts/run-dtcc-modules.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+run_function() {
+    echo "Running $1"
+    if $1; then
+        echo "Successfully excecuted! $1"
+    else
+        echo "Failed to excecuting!! $1"
+    fi
+}
+
+
+files=(wrappers/*.py)
+for file in "${files[@]::${#files[@]}-1}" ; do
+    # Non-blocking background processes
+    echo "Starting $file" 
+    python3 $file &
+done
+# blocking background process
+run_function "python3 ${files[@]: -1:1}"
